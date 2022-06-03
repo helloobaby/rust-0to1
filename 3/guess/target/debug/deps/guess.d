7f234ca677081bf5011f87fc6c9fff45b983c7db
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-06-03 10:01:53 +0800

    rm generated files

--- 3/guess/target/debug/deps/guess.d
+++
@@ -1,5 +0,0 @@
-C:\Users\asdf\Desktop\rust(learn from0to1)\3\guess\target\debug\deps\guess.exe: src\main.rs
-
-C:\Users\asdf\Desktop\rust(learn from0to1)\3\guess\target\debug\deps\guess.d: src\main.rs
-
-src\main.rs:
--- 3/guess/target/debug/guess.d
+++
@@ -1,1 +0,0 @@
-C:\Users\asdf\Desktop\rust(learn\ from0to1)\3\guess\target\debug\guess.exe: C:\Users\asdf\Desktop\rust(learn\ from0to1)\3\guess\src\main.rs
