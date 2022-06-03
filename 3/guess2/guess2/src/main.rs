a8659a5f3497808bc6d300cca1c8d9537018b3b6
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-06-03 12:41:43 +0800

    update

--- 3/guess2/guess2/src/main.rs
+++ 3/guess2/guess2/src/main.rs
@@ -1,13 +1,37 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-println!("Guess the number!");
-let secret_number = rand::thread_rng().gen_range(1..101);
-println!("The secret number is: {}", secret_number);
-println!("Please input your guess.");
-let mut guess = String::new();
-io::stdin()
-.read_line(&mut guess)
-.expect("Failed to read line");
-println!("You guessed: {}", guess);
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+    println!("Please input your guess.");
+
+    //let t;
+
+    let mut guess = String::new(); //guess此时是一个字符串类型
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+    println!("You guessed: {}", guess);
+    
+    //
+    //将guess从字符串转化为u32类型
+
+    //
+    //expect是错误处理,似乎rust强制要求错误处理
+
+    //这里rust有个比c++好的地方，就是能够复用变量名
+    //
+
+    let guess : u32 = guess.trim().parse().expect("Please type a number!\n"); //将guess转化为u32类型
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+    println!("Guess the number!");
+
+
 }
--- 3/readme.md
+++ 3/readme.md
@@ -1,3 +1,4 @@
 用rust写猜猜看游戏
 
+
 cargo doc --open
