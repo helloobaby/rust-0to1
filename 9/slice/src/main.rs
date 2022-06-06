1674c9e4e66c9a4ff924f63bf7340829d6eb0f6a
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-06-06 19:48:09 +0800

    update

--- 9/slice/src/main.rs
+++ 9/slice/src/main.rs
@@ -4,9 +4,22 @@ fn main() {
     let chinese = &str[14..];
 
     //
-    //如果索引不在一个字符的边界上,会出not a char boundary异常
+    //如果索引不在一个字节(char)的边界上,会出not a char boundary异常
+    //c++的索引是按字符来的
     //
     let chinese2 = &str[17..];
+
+    //如何获得指定字符的索引? 类似c++的std::string::find
+    println!("charac index is {}",str.find('中').unwrap());
+
+    //如果出现option为空的话
+    //c++提前要用if判断的
+    //rust用match匹配,match后面的case要跟这个元素的类型(Option? Result?)相匹配
+    let op = str.find('嗨');
+    match op{
+        None => {println!("no 嗨\n");}
+        Some(op) => {println!("index is {}",op);}
+    }
 
     println!("{}",slice);
     println!("{}",chinese);
